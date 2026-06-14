a9eb0d3aea3e5b2b0af6079f94671d32827f2d23
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-14 07:37:06 +0100

    Testing123

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import webview
 
 class Api():
-  def log(self, value):
-    print(value)
+  def ExecuteActions(self, actions):
+    for item in actions:
+      print(item)
+      
+
 
 webview.create_window("BloxxMacro", "https://macro.bloxxer.dev/app", js_api=Api())
 webview.start()
