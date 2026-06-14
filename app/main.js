4713a349bfd3d29464fc88a8abe25bcd64f60f88
Bloxxer126g <tristan.raneroyay@proton.me>
2026-06-14 08:15:57 +0100

    Fix my silly mistake

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 alert("Hello, World!")
 
-document.getElementById("RunButton").addEventListener("click", {
+document.getElementById("RunButton").addEventListener("click", event=>{
     pywebview.api.ExecuteActions(["(20,30)","X"]);
 })
